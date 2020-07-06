3b7ab7d3cbd3020c1f82171101a4fa239d8df456
Giulio Coa <34110430+giulioc008@users.noreply.github.com>
2020-07-06 23:48:40 +0200

    Update main.py

--- main.py
+++ main.py
@@ -54,7 +54,7 @@ with connection.cursor() as cursor:
 	admins_list = list(map(lambda n: n["id"], cursor.fetchall()))
 
 logger.info("Admins setted\nInitializing the Client ...")
-app = Client(session_name=config.get("bot_username"), api_id=config.get("app_id"), api_hash=config.get("app_hash"), bot_token=config.get("bot_token"), lang_code="it")
+app = Client(session_name=config.get("bot_username"), api_id=config.get("app_id"), api_hash=config.get("app_hash"), bot_token=config.get("bot_token"), lang_code="it", workdir=".")
 
 
 @app.on_message(Filters.command("help", prefixes="/") & Filters.user(admins_list) & Filters.private)
